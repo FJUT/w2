25610ace3b95ef625bba78863831365a9d177314
KainyGuo <gt@kainy.cn>
2019-05-06 16:44:17 +0800

    proxy

--- server.js
+++ server.js
@@ -7,13 +7,26 @@ AV.init({
 });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
+// AV.Cloud.useMasterKey();
 
 var app = require('./app');
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+
+const proxy = require('koa-whistle')
+
+proxy.startWhistle({
+  name: 'package.name', // 必填，一般为项目package.json的name字段
+  port: PORT + 10000, // 必填，whistle监听的端口号，一般可以设置为服务器端口号 + 10000，后面讲如何访问该whistle操作界面，可以直接通过web服务器的端口来访问
+  // baseDir: path.join(__dirname, '../project'), // 可选，一般为项目的根目录路径，主要用于内置的whistle加载项目自带的whistle插件
+  username: 'a', // 可选，设置whistle操作界面的用户名
+  password: 'a', // 可选，设置whistle操作界面的密码
+  rules: 'www.test.com 127.0.0.1\nwww.abc.com 1.1.1.1:8080', // 可选，设置whistle的默认规则Default
+  values: {test: 123, abc: 321}, // 可选，设置whistle的Values
+  sockets: 60 // 可选，设置同一个域名whistle的并发请求量，默认为60，一般无需配置
+})
 
 app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
